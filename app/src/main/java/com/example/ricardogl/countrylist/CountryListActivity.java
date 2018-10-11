603ac8f975c84df600dbd2616c9ece3805102a2d
Ricardogll <rikilonga@gmail.com>
2018-10-11 11:20:46 +0200

    New activity to choose country. When click CHOOSE, we go to countryactivity and when we choose a country we return to the previous activity

--- app/src/main/java/com/example/ricardogl/countrylist/CountryListActivity.java
+++ app/src/main/java/com/example/ricardogl/countrylist/CountryListActivity.java
@@ -1,10 +1,12 @@
 package com.example.ricardogl.countrylist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -36,5 +38,17 @@ public class CountryListActivity extends AppCompatActivity {
         country_list_view.setAdapter(adapter);
 
         country_list_view.addItemDecoration((new DividerItemDecoration(this, DividerItemDecoration.VERTICAL)));
+
+        adapter.setOnClickListener(new CountryListAdapter.OnClickListener(){
+
+            @Override
+            public void onClick(int position) {
+                String country = countries.get(position);
+                Intent data = new Intent();
+                data.putExtra("country", country);
+                setResult(RESULT_OK,data);
+                finish();
+            }
+        });
     }
 }
